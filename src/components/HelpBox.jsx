151d50b67c97455596247fed057a135ad4093190
zntb <zenetibi@yahoo.com>
2024-11-29 12:32:12 +0200

    changed to h3

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -4,8 +4,8 @@ import './HelpBox.css';
 
 function HelpBox({ title, text }) {
   return (
-    <article className="help-box">
-      <h2>{title}</h2>
+    <article className='help-box'>
+      <h3>{title}</h3>
       <p>{text}</p>
     </article>
   );
